b77753951ebc3193bb843e11251abbdd5c1e5831
Muhammad Umer Iqbal <46032685+Umer-Iqbal@users.noreply.github.com>
2021-10-21 22:17:43 +0500

    Add files via upload

---
+++ replace_substring_with_new_string.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sed -i 's/<old-word>/<new-word>/g' <file name*.txt>     
+# replace substring with new string # https://www.cyberciti.biz/faq/unix-linux-replace-string-words-in-many-files/
+
